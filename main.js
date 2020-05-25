bd8670ff3d764c5dbe671dc18bed0bd4a48753e1
Neil Floyd <neilfloyd99@gmail.com>
2020-05-25 00:59:24 -0400

    refactored to have a function for each input and calculation

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getValues() {
+/*function getValues() {
     let billAmount = parseInt(document.getElementById('billAmount').value);
     console.log(billAmount);
     
@@ -18,4 +18,48 @@ function getValues() {
 
 function calculate() {
     getValues();
+}
+*/
+
+function getBillAmount() {
+    let billAmount = parseInt(document.getElementById('billAmount').value);
+    console.log(billAmount);
+    return billAmount;
+}
+
+function getService() {
+    let service = parseInt(document.getElementById('service').value);
+    console.log(service);
+    return service;
+}
+
+function getPeople() {
+    let people = parseInt(document.getElementById('numberOfPeople').value);
+    console.log(people);
+    return people;
+}
+
+function calculateTotalTip() {
+    let b = getBillAmount();
+    let s = getService();
+    let tip = b * (s / 100);
+    console.log(tip); 
+    return tip;
+}
+
+function calculateTipPerPerson() {
+    let t = calculateTotalTip();
+    let p = getPeople();
+    let tipPerPerson = t / p;
+    console.log(tipPerPerson);
+    alert('The tip per person is $' + tipPerPerson);
+    return tipPerPerson;
+}
+
+function calculate() {
+    getBillAmount();
+    getService();
+    getPeople();
+    calculateTotalTip();
+    calculateTipPerPerson();
 }
